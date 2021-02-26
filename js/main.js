7a2779519446623b60c4b7941ffa3513707cbc2d
jk090909 <78129173+jk090909@users.noreply.github.com>
2021-02-26 12:33:42 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,21 +2,21 @@
 
 $(function(){
     // main typing text
-    var typingBool = false; 
-    var typingIdx=0; 
-    var typingTxt = $(".typing-txt").text();
+    let typingBool = false; 
+    let typingIdx=0; 
+    let typingTxt = $(".typing-txt").text();
     typingTxt=typingTxt.split("");
     if(typingBool==false){
        typingBool=true; 
        
-       var tyInt = setInterval(typing,100);
+       let tyInt = setInterval(typing,100);
      } 
      
      function typing(){ 
        if(typingIdx<typingTxt.length){
          $(".typing").append(typingTxt[typingIdx]);
          typingIdx++; 
-       } else{ 
+       } else { 
          clearInterval(tyInt);
        } 
      }
